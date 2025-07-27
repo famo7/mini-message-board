9d59827e71f85121103f8aa9435fb28606eb2c42
famo7 <famo1901@gmail.com>
2025-07-27 19:11:03 +0200

    Update port configuration to use environment variable or default to 3000

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('node:path');
 const expressLayouts = require('express-ejs-layouts');
 const { messages } = require('./db.js');
